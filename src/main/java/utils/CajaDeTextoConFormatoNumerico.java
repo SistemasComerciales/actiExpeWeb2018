b2184cb4d96a99a2cc1c0338566839ae98a29ff6
Desarrollo4 <SISTEMASCOMERCIALES@SISTEMASCOMERCI>
2018-03-09 11:50:15 -0500

    ajsdhljasdnlasd

---
+++ src/main/java/utils/CajaDeTextoConFormatoNumerico.java
@@ -0,0 +1,67 @@
+package utils;
+
+import activa.Expendio.modelo.*;
+import java.text.*;
+
+import javax.swing.*;
+import javax.swing.text.*;
+
+public class CajaDeTextoConFormatoNumerico extends JFormattedTextField {
+
+    private static final long serialVersionUID = 2603424525231186727L;
+
+    private int cifras;
+    private Usuario usuario;
+
+    public CajaDeTextoConFormatoNumerico(int cifras, Usuario usuario) {
+        this.cifras = cifras;
+        this.usuario = usuario;
+
+        prepararCampo();
+    }
+
+    private void prepararCampo() {
+        String mask = "";
+        for (int i = 0; i < cifras; i++) {
+            mask += "#";
+        }
+
+        MaskFormatter mascara = null;
+        try {
+            mascara = new MaskFormatter(mask);
+        } catch (ParseException e) {
+            String cod = "Error (4182)";
+            String err = "Ha Ocurrido un error al Aplicar la mascara.";
+            Log.adicionar(e, cod, usuario, err);
+            JOptionPane.showOptionDialog(null, err, cod, JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, null, "aceptar");
+            e.printStackTrace();
+        }
+        mascara.install(this);
+
+        this.setFocusLostBehavior(JFormattedTextField.PERSIST);
+        this.setFont(Letra.fuenteTxt);
+        this.setForeground(Letra.colorTxt);
+        this.setDisabledTextColor(Letra.colorTxtDisable);
+    }
+
+    /**
+     * Metodo encargado de inhabilitar los campos de hora
+     */
+    public void inhabilitar() {
+        this.setEnabled(false);
+    }
+
+    /**
+     * Metodo encargado de habilitar los campos de hora
+     */
+    public void habilitar() {
+        this.setEnabled(true);
+    }
+
+    /**
+     * Metodo encargado de inicializzar la informacion de la hora, txt y combo
+     */
+    public void inicializar() {
+        this.setText("");
+    }
+}
---
+++ src/main/java/utils/CampoCombo.java
@@ -0,0 +1,45 @@
+package utils;
+
+import javax.swing.*;
+
+public class CampoCombo<T> extends JComboBox<T> {
+
+    private static final long serialVersionUID = 8418263158893218114L;
+
+    @SafeVarargs
+    public CampoCombo(T... valores) {
+        super.setFont(Letra.fuenteCombo);
+        super.setForeground(Letra.colorCombo);
+
+        for (T valor : valores) {
+            super.addItem(valor);
+        }
+        super.setSelectedIndex(-1);
+    }
+
+    public static String getComboValue(CampoCombo<String> combo) {
+        int i = combo.getSelectedIndex();
+        String resp = "";
+        if (i != -1) {
+            resp = combo.getItemAt(i);
+        }
+        return resp;
+    }
+
+    public Object getComboValue2() {
+        int i = this.getSelectedIndex();
+        Object resp = null;
+        if (i != -1) {
+            resp = this.getItemAt(i);
+        }
+        return resp;
+    }
+
+    public static String setValueCombo(CampoCombo<String> campo, String valor) {
+        String resp = valor;
+        if (!resp.trim().isEmpty()) {
+            campo.setSelectedItem(resp);
+        }
+        return resp;
+    }
+}
