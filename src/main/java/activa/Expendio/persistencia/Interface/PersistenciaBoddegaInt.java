0ab95c57997f1c2d31b212f79c7af687a9479bdb
Usuario <Usuario@DESARROLLO6>
2018-03-09 10:56:01 -0500

    interfacesss

---
+++ src/main/java/activa/Expendio/persistencia/Interface/PersistenciaBoddegaInt.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activa.Expendio.persistencia.Interface;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface PersistenciaBoddegaInt {
+    
+}
