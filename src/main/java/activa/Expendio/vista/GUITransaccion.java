c7864120cd49940885c04a8d51ff5d65b0640549
diego <diego@DESKTOP-CBKEABN>
2018-03-10 16:53:06 -0500

    asd

---
+++ src/main/java/activa/Expendio/vista/GUITransaccion.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activa.Expendio.vista;
+
+import activa.Expendio.modelo.Establecimiento;
+import activa.Expendio.modelo.Usuario;
+import utils.CargaImagenes;
+import utils.Imagenes;
+import utils.NombreImagenes;
+
+/**
+ *
+ * @author diego
+ */
+public class GUITransaccion extends ClaseGeneral {
+    
+    
+    public GUITransaccion(Usuario usuario, Establecimiento establecimiento){
+        super(usuario, establecimiento);
+        
+        super.tituloFrame(0, CargaImagenes.ALTO_PANTALLA / 100 * 2, "".toUpperCase(), CargaImagenes.ANCHO_PANTALLA, 50);
+
+    }  
+
+        /**
+     * Método encargado del diseño del frame, tamaño, fondo.
+     */
+    protected void prepareElementos() {
+        this.setTitle("Transacciones");
+        Imagenes.fondoInternalFrame(NombreImagenes.imFondoG, this.getWidth(), this.getHeight(), this);
+
+    }
+    
+    @Override
+    public void actualizarFrame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminarReferencia() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void asignarFoco() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void asignarPermisos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
