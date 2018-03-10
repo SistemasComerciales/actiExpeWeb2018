b467cf81eae365e5e36d8484a284d3ad98b57664
diego <diego@DESKTOP-CBKEABN>
2018-03-10 15:48:22 -0500

    ad

--- src/main/java/activa/Expendio/vista/GUIProducto.java
+++ src/main/java/activa/Expendio/vista/GUIProducto.java
@@ -24,6 +24,8 @@ public class GUIProducto extends GUIInterfazCatalogos{
     private CajaDeTexto txt_codigo, txt_nombre, txt_presentacion, txt_grupo, txt_idGrupo, txt_codigoBarras, txt_observaciones;
     private CampoCombo<String> combo_controlExistencia, combo_afectaCupo, combo_controlSerial, combo_precio10Porciento, combo_estado, combo_eliminado;
     
+//    private static int columna
+    
     public GUIProducto(Usuario usuario, Establecimiento establecimiento ) {
 	super(usuario, establecimiento, false);
     }
@@ -66,7 +68,7 @@ public class GUIProducto extends GUIInterfazCatalogos{
         lbl_codigo =new CampoLabel("Codigo" ,"E");
         lbl_codigo.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_codigo.alinearDerecha();
-        lbl_codigo.setLocation(CargaImagenes.ANCHO_PANTALLA/100*10, CargaImagenes.ALTO_PANTALLA/100*14);
+        lbl_codigo.setLocation(CargaImagenes.ANCHO_PANTALLA/100*10, CargaImagenes.ALTO_PANTALLA/100*12);
         this.add(lbl_codigo);
         
         txt_codigo = new CajaDeTexto("G",15);
@@ -77,7 +79,7 @@ public class GUIProducto extends GUIInterfazCatalogos{
         lbl_nombre =new CampoLabel("Nombre" ,"E");
         lbl_nombre.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_nombre.alinearDerecha();
-        lbl_nombre.setLocation(CargaImagenes.ANCHO_PANTALLA/100*27, CargaImagenes.ALTO_PANTALLA/100*14);
+        lbl_nombre.setLocation(CargaImagenes.ANCHO_PANTALLA/100*27, CargaImagenes.ALTO_PANTALLA/100*12);
         this.add(lbl_nombre);
         
         txt_nombre = new CajaDeTexto("G",15);
@@ -88,7 +90,7 @@ public class GUIProducto extends GUIInterfazCatalogos{
         lbl_presentacion =new CampoLabel("Presentación" ,"E");
         lbl_presentacion.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_presentacion.alinearDerecha();
-        lbl_presentacion.setLocation(CargaImagenes.ANCHO_PANTALLA/100*50, CargaImagenes.ALTO_PANTALLA/100*14);
+        lbl_presentacion.setLocation(CargaImagenes.ANCHO_PANTALLA/100*50, CargaImagenes.ALTO_PANTALLA/100*12);
         this.add(lbl_presentacion);
         
         txt_presentacion = new CajaDeTexto("G",15);
@@ -99,7 +101,7 @@ public class GUIProducto extends GUIInterfazCatalogos{
         lbl_controlExistencia =new CampoLabel("Control Existencia" ,"E");
         lbl_controlExistencia.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_controlExistencia.alinearDerecha();
-        lbl_controlExistencia.setLocation(CargaImagenes.ANCHO_PANTALLA/100*27, CargaImagenes.ALTO_PANTALLA/100*25);
+        lbl_controlExistencia.setLocation(lbl_codigo.getX(), CargaImagenes.ALTO_PANTALLA/100*18);
         this.add(lbl_controlExistencia);
         
         combo_controlExistencia = new CampoCombo<String>(DatosBaseDatos.varNo, DatosBaseDatos.varSi);
@@ -107,7 +109,7 @@ public class GUIProducto extends GUIInterfazCatalogos{
         combo_controlExistencia.setSize(lbl_controlExistencia.getWidth() * 8 / 9, lbl_controlExistencia.getHeight());
         this.add(combo_controlExistencia);
         
-        lbl_afectaCupo =new CampoLabel("Afecta cupo" ,"E");
+        lbl_afectaCupo =new CampoLabel("A. cupo" ,"E");
         lbl_afectaCupo.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_afectaCupo.alinearDerecha();
         lbl_afectaCupo.setLocation(lbl_nombre.getX(), lbl_controlExistencia.getY());
@@ -118,7 +120,7 @@ public class GUIProducto extends GUIInterfazCatalogos{
         combo_afectaCupo.setSize(lbl_afectaCupo.getWidth() * 8 / 9, lbl_afectaCupo.getHeight());
         this.add(combo_afectaCupo);
         
-        lbl_controlSerial =new CampoLabel("Afecta cupo" ,"E");
+        lbl_controlSerial =new CampoLabel("Control serial" ,"E");
         lbl_controlSerial.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_controlSerial.alinearDerecha();
         lbl_controlSerial.setLocation(lbl_presentacion.getX(), lbl_controlExistencia.getY());
@@ -129,21 +131,10 @@ public class GUIProducto extends GUIInterfazCatalogos{
         combo_controlSerial.setSize(lbl_controlSerial.getWidth() * 8 / 9, lbl_controlSerial.getHeight());
         this.add(combo_controlSerial);
         
-        lbl_precio10Porciento =new CampoLabel("Afecta cupo" ,"E");
-        lbl_precio10Porciento.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
-        lbl_precio10Porciento.alinearDerecha();
-        lbl_precio10Porciento.setLocation(lbl_controlExistencia.getX(), CargaImagenes.ALTO_PANTALLA/100*40);
-        this.add(lbl_precio10Porciento);
-        
-        combo_precio10Porciento = new CampoCombo<String>(DatosBaseDatos.varNo, DatosBaseDatos.varSi);
-        combo_precio10Porciento.setLocation(lbl_precio10Porciento.getX()+lbl_precio10Porciento.getWidth()*5/4, lbl_precio10Porciento.getY());
-        combo_precio10Porciento.setSize(lbl_precio10Porciento.getWidth() * 8 / 9, lbl_precio10Porciento.getHeight());
-        this.add(combo_precio10Porciento);
-        
         lbl_precio10Porciento =new CampoLabel("Precio 10%" ,"E");
         lbl_precio10Porciento.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_precio10Porciento.alinearDerecha();
-        lbl_precio10Porciento.setLocation(lbl_afectaCupo.getX(), CargaImagenes.ALTO_PANTALLA/100*40);
+        lbl_precio10Porciento.setLocation(lbl_controlExistencia.getX(), CargaImagenes.ALTO_PANTALLA/100*24);
         this.add(lbl_precio10Porciento);
         
         combo_precio10Porciento = new CampoCombo<String>(DatosBaseDatos.varNo, DatosBaseDatos.varSi);
@@ -151,16 +142,50 @@ public class GUIProducto extends GUIInterfazCatalogos{
         combo_precio10Porciento.setSize(lbl_precio10Porciento.getWidth() * 8 / 9, lbl_precio10Porciento.getHeight());
         this.add(combo_precio10Porciento);
         
-        lbl_estado =new CampoLabel("Precio 10%" ,"E");
+        lbl_codigoBarras =new CampoLabel("Cod barras" ,"E");
+        lbl_codigoBarras.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
+        lbl_codigoBarras.alinearDerecha();
+        lbl_codigoBarras.setLocation(lbl_nombre.getX(), lbl_precio10Porciento.getY());
+        this.add(lbl_codigoBarras);
+        
+        txt_codigoBarras = new CajaDeTexto("G",15);
+        txt_codigoBarras.setLocation(lbl_codigoBarras.getX()+lbl_codigoBarras.getWidth()*5/4, lbl_codigoBarras.getY());
+        txt_codigoBarras.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
+        this.add(txt_codigoBarras);
+        
+        lbl_estado =new CampoLabel("Estado" ,"E");
         lbl_estado.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_estado.alinearDerecha();
-        lbl_estado.setLocation(lbl_controlSerial.getX(), CargaImagenes.ALTO_PANTALLA/100*40);
+        lbl_estado.setLocation(lbl_controlSerial.getX(), lbl_precio10Porciento.getY());
         this.add(lbl_estado);
         
         combo_estado = new CampoCombo<String>(DatosBaseDatos.estadoActivo, DatosBaseDatos.estadoInactivo);
-        combo_estado.setLocation(lbl_precio10Porciento.getX()+lbl_precio10Porciento.getWidth()*5/4, lbl_precio10Porciento.getY());
-        combo_estado.setSize(lbl_precio10Porciento.getWidth() * 8 / 9, lbl_precio10Porciento.getHeight());
+        combo_estado.setLocation(lbl_estado.getX()+lbl_estado.getWidth()*5/4, lbl_estado.getY());
+        combo_estado.setSize(lbl_estado.getWidth() * 8 / 9, lbl_estado.getHeight());
         this.add(combo_estado);
+        
+        lbl_eliminado =new CampoLabel("Eliminado" ,"E");
+        lbl_eliminado.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
+        lbl_eliminado.alinearDerecha();
+        lbl_eliminado.setLocation(lbl_precio10Porciento.getX(),  CargaImagenes.ALTO_PANTALLA/100*30);
+        this.add(lbl_eliminado);
+        
+        combo_eliminado = new CampoCombo<String>(DatosBaseDatos.estadoActivo, DatosBaseDatos.estadoInactivo);
+        combo_eliminado.setLocation(lbl_eliminado.getX()+lbl_eliminado.getWidth()*5/4, lbl_eliminado.getY());
+        combo_eliminado.setSize(lbl_eliminado.getWidth() * 8 / 9, lbl_eliminado.getHeight());
+        this.add(combo_eliminado);
+        
+        lbl_observacion =new CampoLabel("Nota" ,"E");
+        lbl_observacion.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
+        lbl_observacion.alinearDerecha();
+        lbl_observacion.setLocation(lbl_codigoBarras.getX(), lbl_eliminado.getY());
+        this.add(lbl_observacion);
+        
+        txt_observaciones = new CajaDeTexto("G",15);
+        txt_observaciones.setLocation(lbl_observacion.getX()+lbl_observacion.getWidth()*5/4, lbl_observacion.getY());
+        txt_observaciones.setSize(CargaImagenes.anchoBotonCatalogos , 30);
+        this.add(txt_observaciones);
+        
     }
 
     @Override
