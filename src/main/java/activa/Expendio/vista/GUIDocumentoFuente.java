b887ef63162307d0d5f69181a405d3880a2ebd38
diego <diego@DESKTOP-CBKEABN>
2018-03-10 15:20:17 -0500

    d

--- src/main/java/activa/Expendio/vista/GUIDocumentoFuente.java
+++ src/main/java/activa/Expendio/vista/GUIDocumentoFuente.java
@@ -10,8 +10,15 @@ import activa.Expendio.modelo.Establecimiento;
 import activa.Expendio.modelo.Usuario;
 import activa.Expendio.vista.utils.CajaDeTexto;
 import activa.Expendio.vista.utils.CampoLabel;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import utils.CampoCombo;
 import utils.CargaImagenes;
+import utils.Tabla;
+import utils.TablaNoEditable;
 
 /**
  *
@@ -22,12 +29,95 @@ public class GUIDocumentoFuente extends GUIInterfazCatalogos {
     private CampoLabel lbl_codigo , lbl_nombre, lbl_accion, lbl_aplica, lbl_numera, lbl_numero, lbl_controlExiistencia, lbl_docBase, lbl_precioFijo, lbl_listaPrecio, lbl_costeoInv, lbl_bodega, lbl_estado, lbl_eliminado; 
     private CajaDeTexto txt_codigo, txt_nombre, txt_numero, txt_idBodega, txt_bodega;
     private CampoCombo<String> combo_accionInv, combo_aplica, combo_numera, combo_controlExis, combo_docBase, combo_precioFijo, combo_listaPrecio, combo_costeoInv, combo_estado, combo_eliminado;
+    
+    public static int columnaCodigo = 0 ;
+    public static int columnaNombre = 1 ;
+    public static int columnaAccionInv = 2 ;
+    public static int columnaAplica = 3 ;
+    public static int columnaNumera = 4 ;
+    public static int columnaNumero = 5 ;
+    public static int columnaControlExitencia = 6 ;
+    public static int columnaDocBase = 7 ;
+    public static int columnaPrecioFijo = 8 ;
+    public static int columnaListaPrecio = 9 ;
+    public static int columnaCosteoInv = 10 ;
+    public static int columnaBodega = 11 ;
+    public static int columnaIdBodega = 12 ;
+    public static int columnaEstado = 13 ;
+    public static int columnaEliminado = 14 ;
+    public static int columnaId = 15 ;
+    
+    /**
+     * constructor
+     * @param usuario
+     * @param establecimiento 
+     */
     public GUIDocumentoFuente(Usuario usuario, Establecimiento establecimiento) {
         super(usuario, establecimiento, false);
     }
 
     @Override
     protected void prepareElementosTablaGeneral() {
+        int margenIzquierda = 0;
+        int margenSuperior = CargaImagenes.ALTO_PANTALLA/4+CargaImagenes.ALTO_PANTALLA/6;
+        int anchoPanel = 80*(CargaImagenes.ANCHO_PANTALLA/100);
+        int altoPanel = ((CargaImagenes.ALTO_PANTALLA/2));
+
+        margenIzquierda = (CargaImagenes.ANCHO_PANTALLA - anchoPanel)/2;
+        dtmTablaGeneral= new TablaNoEditable();
+        tablaGeneral = new JTable(dtmTablaGeneral);
+        panel_tablaGeneral=new JPanel();
+        panel_tablaGeneral.setOpaque(false);
+        panel_tablaGeneral.setBounds(margenIzquierda,margenSuperior, anchoPanel,altoPanel) ;
+        dtmTablaGeneral.addColumn("Código");//1
+        dtmTablaGeneral.addColumn("Nombre");//2
+        dtmTablaGeneral.addColumn("Accion inventario");//3
+        dtmTablaGeneral.addColumn("Aplica a");//4
+        dtmTablaGeneral.addColumn("Numera");//5
+        dtmTablaGeneral.addColumn("Numero");//6
+        dtmTablaGeneral.addColumn("C. existencia");//7
+        dtmTablaGeneral.addColumn("Doc Base");//8
+        dtmTablaGeneral.addColumn("Precio fijo");//9
+        dtmTablaGeneral.addColumn("lista precio");//10
+        dtmTablaGeneral.addColumn("Costeo inv");//11
+        dtmTablaGeneral.addColumn("Bodega");//12
+        dtmTablaGeneral.addColumn("idBodega");//13
+        dtmTablaGeneral.addColumn("Estado");//14
+        dtmTablaGeneral.addColumn("Eliminado");//15
+        dtmTablaGeneral.addColumn("Id");//16
+
+        
+        tablaGeneral.setPreferredScrollableViewportSize(new Dimension(anchoPanel-25,altoPanel-27));
+        scrollPaneTablaGeneral= new JScrollPane(tablaGeneral);
+        panel_tablaGeneral.add(scrollPaneTablaGeneral);
+        scrollPaneTablaGeneral.setBounds(0,0 , anchoPanel-25,altoPanel-27);
+        this.add(panel_tablaGeneral);
+
+
+
+        tablaGeneral.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tablaGeneral.getTableHeader().setReorderingAllowed(false);
+        tablaGeneral.setDefaultRenderer(Object.class, new Tabla.MiRender());
+        tablaGeneral.setShowHorizontalLines(false);
+        tablaGeneral.setBorder(null);
+        tablaGeneral.setOpaque(false);
+        panel_tablaGeneral.setOpaque(false);
+        panel_tablaGeneral.setBorder(null);
+        scrollPaneTablaGeneral.setOpaque(false);
+        scrollPaneTablaGeneral.getViewport().setOpaque(false);
+        scrollPaneTablaGeneral.setBorder(null);      
+
+        int anchoTotal = anchoPanel/20;
+        tablaGeneral.getColumnModel().getColumn(columnaCodigo).setPreferredWidth(anchoTotal);
+        tablaGeneral.getColumnModel().getColumn(columnaNombre).setPreferredWidth(3*anchoTotal);
+        
+        tablaGeneral.getColumnModel().getColumn(columnaIdBodega).setMaxWidth(0);
+        tablaGeneral.getColumnModel().getColumn(columnaIdBodega).setMinWidth(0);
+        tablaGeneral.getColumnModel().getColumn(columnaIdBodega).setPreferredWidth(0);
+        
+        tablaGeneral.getColumnModel().getColumn(columnaId).setMaxWidth(0);
+        tablaGeneral.getColumnModel().getColumn(columnaId).setMinWidth(0);
+        tablaGeneral.getColumnModel().getColumn(columnaId).setPreferredWidth(0);
     }
 
     @Override
@@ -171,7 +261,7 @@ public class GUIDocumentoFuente extends GUIInterfazCatalogos {
         combo_listaPrecio.setSize(lbl_listaPrecio.getWidth() * 8 / 9, lbl_listaPrecio.getHeight());
         this.add(combo_listaPrecio);
         
-        lbl_costeoInv =new CampoLabel("L. precio" ,"E");
+        lbl_costeoInv =new CampoLabel("Coste inv." ,"E");
         lbl_costeoInv.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_costeoInv.alinearDerecha();
         lbl_costeoInv.setLocation(lbl_docBase.getX(), lbl_listaPrecio.getY());
@@ -205,16 +295,16 @@ public class GUIDocumentoFuente extends GUIInterfazCatalogos {
         combo_estado.setSize(lbl_estado.getWidth() * 8 / 9, lbl_estado.getHeight());
         this.add(combo_estado);
         
-        lbl_eliminado  =new CampoLabel("" ,"E");
+        lbl_eliminado  =new CampoLabel("Eliminado" ,"E");
         lbl_eliminado.setSize(CargaImagenes.anchoBotonCatalogos/2 , 30);
         lbl_eliminado.alinearDerecha();
-        lbl_eliminado.setLocation(lbl_listaPrecio.getX() , CargaImagenes.ALTO_PANTALLA/100*30);
+        lbl_eliminado.setLocation(lbl_costeoInv.getX() , CargaImagenes.ALTO_PANTALLA/100*30);
         this.add(lbl_eliminado);
         
-        combo_estado = new CampoCombo<String>(DatosBaseDatos.estadoActivo, DatosBaseDatos.estadoInactivo);
-        combo_estado.setLocation(lbl_estado.getX()+lbl_estado.getWidth()*5/4, lbl_estado.getY());
-        combo_estado.setSize(lbl_estado.getWidth() * 8 / 9, lbl_estado.getHeight());
-        this.add(combo_estado);
+        combo_eliminado = new CampoCombo<String>(DatosBaseDatos.varSi, DatosBaseDatos.varNo);
+        combo_eliminado.setLocation(lbl_eliminado.getX()+lbl_eliminado.getWidth()*5/4, lbl_eliminado.getY());
+        combo_eliminado.setSize(lbl_eliminado.getWidth() * 8 / 9, lbl_eliminado.getHeight());
+        this.add(combo_eliminado);
 
     }
 
