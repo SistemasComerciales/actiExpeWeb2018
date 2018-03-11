82c6265e445771d7a883873dc113c36f510e451c
diego <diego@DESKTOP-CBKEABN>
2018-03-10 19:17:23 -0500

    asdwqqwwqw

--- src/main/java/activa/Expendio/vista/GUITransaccion.java
+++ src/main/java/activa/Expendio/vista/GUITransaccion.java
@@ -7,9 +7,25 @@ package activa.Expendio.vista;
 
 import activa.Expendio.modelo.Establecimiento;
 import activa.Expendio.modelo.Usuario;
+import activa.Expendio.vista.utils.CajaDeTexto;
+import activa.Expendio.vista.utils.CajaDeTextoConFormato;
+import activa.Expendio.vista.utils.CampoLabel;
+import java.awt.Dimension;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+import utils.CampoCombo;
 import utils.CargaImagenes;
 import utils.Imagenes;
 import utils.NombreImagenes;
+import utils.Tabla.MiRender;
+import utils.TablaNoEditable;
 
 /**
  *
@@ -17,10 +33,41 @@ import utils.NombreImagenes;
  */
 public class GUITransaccion extends ClaseGeneral {
     
+    public CampoLabel lbl_nombreDocFuente;
+    
+    private CampoLabel lbl_documento , lbl_numero, lbl_fecha , lbl_nit, lbl_condiciones , lbl_nombre , lbl_nombreDin, lbl_direccion, lbl_direccionDin, lbl_ciudad , lbl_ciudadDin , lbl_telefono , lbl_telefonoDin , lbl_numDocumentoTercero;
+    private CajaDeTexto txt_fechaAntigua, txt_idDocumentoFuente, txt_documento, txt_documentoAntiguo, txt_numeracion, txt_numeroFijo ,txt_documentoCierre, txt_numero, txt_estado ,  txt_idTercero, txt_nit ,txt_nitAntiguo, txt_idTipoRegimen ,txt_nombreTipoRegimen , txt_codigoTipoRegimen , txt_asumeReteIvaTipoRegimen,  txt_condiciones, txt_numDocumento,  txt_idVendedor, txt_vendedor, txt_llevaVendedor, txt_TipoClienteDocFuente , txt_descuentoDocFuente , txt_TipoDescuentoDOcFuente, txt_aplicaRetenciones, txt_aplicaIvaDocFuente, txt_llevaBodegaDocFuente, txt_idBodegaDocFuente, txt_accionSobreInventario, txt_costeKardex, txt_interfaceDocFuente, txt_llevaClienteFijo, txt_listaPrecioDoc, txt_listaPrecioTercero , txt_ControlExistenciaDocFuente, txt_preCostoDocFuente,txt_cuentasPorCobrar, txt_cuentasPorPagar, txt_idTransaccionOriginal, txt_esAutoRete, txt_ControlExistenciaProducto;
+    private CajaDeTextoConFormato txt_fecha;
+    
+        ////////////panel REGISTRO2 MVTRANSACCION///////////
+    private CampoLabel lbl_codigo, lbl_descripcion, lbl_empaque, lbl_bodega , lbl_cantidad , lbl_valorUnitario  , lbl_valorTolal; 	
+    private CajaDeTexto txt_idCodigo, txt_codigo,  txt_descripcion, txt_empaque, txt_idBodega , txt_bodega , txt_idTransaccion;
+    private CajaDeTexto txt_cantidad , txt_ValorUnitario , txt_valorTotal;
+    
+    ///////////////////PANEL PREPARA TABLA PRINCIPAL///////////////////////
+    public JPanel panel_tablaPrincipal;
+    public DefaultTableModel dtmTablaPrincipal;
+    public JTable tablaPrincipal;
+    public JScrollPane scrollpanePrincipal;
+    public String DatosTablaPrincipal[];
+    public TableRowSorter<DefaultTableModel> trsFiltrotablaPrincipal;
+    
+    public static int columnaCodigo =0;
+    public static int columnaDescripcion =1;
+    public static int columnaEmpaque =2;
+    public static int columnaBodega =3;
+    public static int columnaCantidad =4;
+    public static int columnaVunitario =5;
+    public static int columnaVtotal =6;
+    public static int columnaidBodega =7;
+  
     
     public GUITransaccion(Usuario usuario, Establecimiento establecimiento){
         super(usuario, establecimiento);
-        
+        prepareElementos();
+        prepareElementosInfo();
+        preparaElementosTablaPrincipal();
+        prepararElementosRegistro2();
         super.tituloFrame(0, CargaImagenes.ALTO_PANTALLA / 100 * 2, "".toUpperCase(), CargaImagenes.ANCHO_PANTALLA, 50);
 
     }  
@@ -32,6 +79,307 @@ public class GUITransaccion extends ClaseGeneral {
         this.setTitle("Transacciones");
         Imagenes.fondoInternalFrame(NombreImagenes.imFondoG, this.getWidth(), this.getHeight(), this);
     }
+    
+    /**
+     * 
+     */
+    private void prepareElementosInfo(){
+
+            int posicionXTxt = CargaImagenes.ANCHO_PANTALLA/100*13 ;
+            int posicionYtxt = CargaImagenes.ALTO_PANTALLA/100*4-5;
+            int posicionXLbl = CargaImagenes.ANCHO_PANTALLA/100*6;
+            int posicionYLbl = CargaImagenes.ALTO_PANTALLA/100;
+		
+		
+		/////////////////////////LABEL/////////////////////
+
+        lbl_nombreDocFuente =new CampoLabel("" ,"V");
+        lbl_nombreDocFuente.alinearIzquierda();
+        lbl_nombreDocFuente.setBounds(posicionXLbl, posicionYLbl, CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_nombreDocFuente);
+
+        lbl_documento = new CampoLabel("Documento:  ", "E");
+        lbl_documento.alinearIzquierda();
+        lbl_documento.setBounds(posicionXLbl, lbl_nombreDocFuente.getY()+lbl_nombreDocFuente.getHeight(), CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_documento);
+
+
+        lbl_numero = new CampoLabel("Número:  ", "E");
+        lbl_numero.alinearIzquierda();
+        lbl_numero.setBounds(posicionXLbl, lbl_documento.getY()+lbl_documento.getHeight()+5, CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_numero);
+
+        lbl_fecha = new CampoLabel("Fecha:  ", "E");
+        lbl_fecha.alinearIzquierda();
+        lbl_fecha.setBounds(posicionXLbl, lbl_numero.getY()+lbl_numero.getHeight()+5, CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_fecha);
+
+
+        lbl_nit = new CampoLabel("NIT/c.c:  ", "E");
+        lbl_nit.alinearIzquierda();
+        lbl_nit.setBounds(posicionXLbl, lbl_fecha.getY()+lbl_fecha.getHeight()+5, CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_nit);
+
+
+        lbl_condiciones = new CampoLabel("Condiciones:  ", "E");
+        lbl_condiciones.alinearIzquierda();
+        lbl_condiciones.setBounds(posicionXLbl, lbl_nit.getY()+lbl_nit.getHeight()+5, CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_condiciones);
+
+
+        lbl_nombre = new CampoLabel("Tercero:  ", "E");
+        lbl_nombre.alinearIzquierda();
+        lbl_nombre.setBounds(lbl_documento.getX()*7/2+lbl_documento.getWidth(), lbl_documento.getY(), CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_nombre);
+
+
+        lbl_nombreDin = new CampoLabel("", "V");
+        lbl_nombreDin.alinearIzquierda();
+        lbl_nombreDin.setBounds(lbl_nombre.getX()+lbl_nombre.getWidth(), lbl_nombre.getY(), CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_nombreDin);
+
+
+        lbl_direccion = new CampoLabel("Direccion:  ", "E");
+        lbl_direccion.alinearIzquierda();
+        lbl_direccion.setBounds(lbl_nombre.getX(), lbl_nombre.getY()+lbl_nombre.getHeight()+5, CargaImagenes.anchoBotonGeneral, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_direccion);
+
+
+        lbl_direccionDin = new CampoLabel("", "V");
+        lbl_direccionDin.alinearIzquierda();
+        lbl_direccionDin.setBounds(lbl_nombreDin.getX(), lbl_nombre.getY()+lbl_nombre.getHeight()+5, CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_direccionDin);
+
+
+        lbl_ciudad = new CampoLabel("Ciudad:  ", "E");
+        lbl_ciudad.alinearIzquierda();
+        lbl_ciudad.setBounds(lbl_nombre.getX(), lbl_direccion.getY()+lbl_direccion.getHeight()+5, CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_ciudad);
+
+
+        lbl_ciudadDin = new CampoLabel("", "V");
+        lbl_ciudadDin.alinearIzquierda();
+        lbl_ciudadDin.setBounds(lbl_direccionDin.getX(), lbl_direccion.getY()+lbl_direccion.getHeight()+5, CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_ciudadDin);
+
+
+        lbl_telefono = new CampoLabel("Teléfono:  ", "E");
+        lbl_telefono.alinearIzquierda();
+        lbl_telefono.setBounds(lbl_nombre.getX(), lbl_ciudad.getY()+lbl_ciudad.getHeight()+5, CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_telefono);
+
+        lbl_telefonoDin = new CampoLabel("", "V");
+        lbl_telefonoDin.alinearIzquierda();
+        lbl_telefonoDin.setBounds(lbl_ciudadDin.getX(), lbl_ciudad.getY()+lbl_ciudad.getHeight()+5, CargaImagenes.anchoBotonGeneral*2, CargaImagenes.altoBotonGeneral/2);
+        this.add(lbl_telefonoDin);
+
+
+
+
+
+                //////////////////JTEXTFIELD////////////////////////////
+
+
+        txt_idDocumentoFuente = new CajaDeTexto("G"); 
+
+        
+        txt_documento = new CajaDeTexto("G",15);
+        txt_documento.setBounds(posicionXTxt, posicionYtxt, CargaImagenes.anchoBotonGeneral, 20);
+        this.add(txt_documento);
+
+        txt_TipoClienteDocFuente = new CajaDeTexto("G");
+        txt_numeracion = new CajaDeTexto("G");
+        txt_numeroFijo = new CajaDeTexto("G");
+        txt_documentoCierre = new CajaDeTexto("G");
+        txt_documentoAntiguo = new CajaDeTexto("G");
+
+        txt_numero = new CajaDeTexto("G",15);
+        txt_numero.setBounds(posicionXTxt, lbl_numero.getY(), CargaImagenes.anchoBotonGeneral, 20);
+        this.add(txt_numero);
+
+        txt_fecha = new CajaDeTextoConFormato(CajaDeTexto.textoFecha, usuario);
+        txt_fecha.setBounds(posicionXTxt, lbl_fecha.getY(), CargaImagenes.anchoBotonGeneral, 20);
+        this.add(txt_fecha);
+ 
+        txt_idTercero = new CajaDeTexto("G");
+        txt_nitAntiguo = new CajaDeTexto("G");
+        txt_ControlExistenciaProducto = new CajaDeTexto("G");
+        
+        
+        txt_nit = new CajaDeTexto("G",50);
+        txt_nit.setBounds(posicionXTxt, lbl_nit.getY(), CargaImagenes.anchoBotonGeneral, 20);
+        this.add(txt_nit);
+
+        txt_condiciones = new CajaDeTexto("G",500);
+        txt_condiciones.setBounds(posicionXTxt, lbl_condiciones.getY(), CargaImagenes.anchoBotonGeneral+CargaImagenes.anchoBotonGeneral/2, 20);
+        this.add(txt_condiciones);
+
+
+    }
+    
+    	/**
+	 * @param metodo que prepara los elementos de registro 2 
+	 */
+	public void prepararElementosRegistro2(){
+
+		int posicionXTxt = panel_tablaPrincipal.getX();
+		int posicionYTxt = CargaImagenes.ALTO_PANTALLA/100*27;
+		int posicionYlbl = CargaImagenes.ALTO_PANTALLA/100*23;
+		
+		lbl_codigo = new CampoLabel("Código", "E");
+		lbl_codigo.alinearCentro();
+		lbl_codigo.setBounds(posicionXTxt, posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaCodigo).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_codigo);
+		
+
+		lbl_descripcion = new CampoLabel("Descripción  ", "E");
+		lbl_descripcion.alinearCentro();
+		lbl_descripcion.setBounds(lbl_codigo.getX()+lbl_codigo.getWidth(), posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaDescripcion).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_descripcion);
+		
+		
+		lbl_empaque = new CampoLabel("Empaque", "E");
+		lbl_empaque.alinearCentro();
+		lbl_empaque.setBounds(lbl_descripcion.getX()+lbl_descripcion.getWidth(), posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaEmpaque).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_empaque);
+		
+		lbl_bodega = new CampoLabel("Bodega", "E");
+		lbl_bodega.alinearCentro();
+		lbl_bodega.setBounds(lbl_empaque.getX()+lbl_empaque.getWidth(), posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaBodega).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_bodega);
+		
+		
+		lbl_cantidad = new CampoLabel("Cantidad", "E");
+		lbl_cantidad.alinearCentro();
+		lbl_cantidad.setBounds(lbl_bodega.getX()+lbl_bodega.getWidth(), posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaCantidad).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_cantidad);
+		
+		lbl_valorUnitario = new CampoLabel("Valor Unitario  ", "E");
+		lbl_valorUnitario.alinearCentro();
+		lbl_valorUnitario.setBounds(lbl_cantidad.getX()+lbl_cantidad.getWidth(), posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaVunitario).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_valorUnitario);
+		
+		
+		lbl_valorTolal = new CampoLabel("Valor Total  ", "E");
+		lbl_valorTolal.alinearCentro();
+		lbl_valorTolal.setBounds(lbl_valorUnitario.getX()+lbl_valorUnitario.getWidth(), posicionYlbl, tablaPrincipal.getColumnModel().getColumn(columnaVtotal).getMaxWidth(), CargaImagenes.altoBotonGeneral/5*4);
+		this.add(lbl_valorTolal);
+
+		
+		txt_idTransaccion = new CajaDeTexto("G");
+		txt_idCodigo = new CajaDeTexto("G");
+		
+		
+		txt_codigo = new CajaDeTexto("G", 20);
+		txt_codigo.setBounds(posicionXTxt, posicionYTxt, tablaPrincipal.getColumnModel().getColumn(1).getMaxWidth(), 20);
+		this.add(txt_codigo);
+		
+		txt_descripcion = new CajaDeTexto("G", 20);
+		txt_descripcion.setBounds(txt_codigo.getX()+txt_codigo.getWidth(), posicionYTxt, tablaPrincipal.getColumnModel().getColumn(2).getMaxWidth(), 20);
+		this.add(txt_descripcion);
+		txt_descripcion.setEnabled(false);
+		
+		txt_empaque = new CajaDeTexto("G", 20);
+		txt_empaque.setBounds(txt_descripcion.getX()+txt_descripcion.getWidth(), posicionYTxt, tablaPrincipal.getColumnModel().getColumn(3).getMaxWidth(), 20);
+		this.add(txt_empaque);
+		txt_empaque.setEnabled(false);
+		
+		txt_idBodega = new CajaDeTexto("G", 20);
+		txt_bodega = new CajaDeTexto("G", 20);
+		this.add(txt_bodega);
+	
+		txt_cantidad = new CajaDeTexto("D", 20);
+		txt_cantidad.setBounds(txt_bodega.getX()+txt_bodega.getWidth(), posicionYTxt, tablaPrincipal.getColumnModel().getColumn(6).getMaxWidth(), 20);
+		this.add(txt_cantidad);
+
+		txt_ValorUnitario = new CajaDeTexto("M", 20);
+		txt_ValorUnitario.setBounds(txt_cantidad.getX()+txt_cantidad.getWidth(), posicionYTxt, tablaPrincipal.getColumnModel().getColumn(7).getMaxWidth(), 20);
+		this.add(txt_ValorUnitario);
+		
+		txt_valorTotal = new CajaDeTexto("M", 20);
+		txt_valorTotal.setBounds(txt_ValorUnitario.getX()+txt_ValorUnitario.getWidth(), posicionYTxt, tablaPrincipal.getColumnModel().getColumn(8).getMaxWidth(), 20);
+		txt_valorTotal.setEnabled(false);
+		this.add(txt_valorTotal);
+	}
+
+    
+    	/**
+	 *  metodo que prepara la tabla de principal 
+	 */
+	private void preparaElementosTablaPrincipal() {
+
+		panel_tablaPrincipal = new JPanel();
+		panel_tablaPrincipal.setBounds(CargaImagenes.ANCHO_PANTALLA/40, CargaImagenes.ALTO_PANTALLA/40*12 , (CargaImagenes.ANCHO_PANTALLA / 40)*38, CargaImagenes.ALTO_PANTALLA / 40*12);
+		panel_tablaPrincipal.setLayout(null);
+		panel_tablaPrincipal.setBorder(null);
+		panel_tablaPrincipal.setOpaque(false);
+		//panel_tablaPrincipal.setBackground(Color.red);
+		this.add(panel_tablaPrincipal);
+
+		
+
+		dtmTablaPrincipal = new TablaNoEditable();
+		tablaPrincipal = new JTable(dtmTablaPrincipal);
+		dtmTablaPrincipal.addColumn("Código");//0
+		dtmTablaPrincipal.addColumn("Descripción");//1
+		dtmTablaPrincipal.addColumn("Empaque");//2
+		dtmTablaPrincipal.addColumn("Bodega");//3
+		dtmTablaPrincipal.addColumn("Cantidad");//4
+		dtmTablaPrincipal.addColumn("Valor Unitario");//5
+                dtmTablaPrincipal.addColumn("Valor Total");//6
+		dtmTablaPrincipal.addColumn("idBodega");//7
+                
+		tablaPrincipal.setPreferredScrollableViewportSize(new Dimension(CargaImagenes.ANCHO_PANTALLA - 80,(CargaImagenes.ALTO_PANTALLA - (CargaImagenes.ALTO_PANTALLA / 6) - (CargaImagenes.ALTO_PANTALLA / 4)) - 80));
+		scrollpanePrincipal = new JScrollPane(tablaPrincipal);
+		tablaPrincipal.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		panel_tablaPrincipal.add(scrollpanePrincipal);
+		scrollpanePrincipal.setSize(panel_tablaPrincipal.getWidth(), panel_tablaPrincipal.getHeight());
+		scrollpanePrincipal.setLocation(0, 0);
+		tablaPrincipal.getTableHeader().setReorderingAllowed(false);
+
+		tablaPrincipal.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tablaPrincipal.setDefaultRenderer(Object.class,new MiRender());
+		tablaPrincipal.setShowHorizontalLines(false);
+		tablaPrincipal.setBorder(null);
+		tablaPrincipal.setOpaque(false);
+		scrollpanePrincipal.setOpaque(false);
+		scrollpanePrincipal.getViewport().setOpaque(false);
+		scrollpanePrincipal.setBorder(null);
+		tablaPrincipal.getTableHeader().setPreferredSize(new Dimension(10000, 32));
+
+
+
+		//int columna1=ANCHO_PANTALLA/130;
+		int columna1=CargaImagenes.ANCHO_PANTALLA/24;
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaCodigo).setMaxWidth(columna1*2);
+        tablaPrincipal.getColumnModel().getColumn(columnaCodigo).setMinWidth(columna1*1);
+        tablaPrincipal.getColumnModel().getColumn(columnaCodigo).setPreferredWidth(columna1*2);
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaDescripcion).setMaxWidth(columna1*7);
+        tablaPrincipal.getColumnModel().getColumn(columnaDescripcion).setMinWidth(columna1*4);
+        tablaPrincipal.getColumnModel().getColumn(columnaDescripcion).setPreferredWidth(columna1*7);
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaEmpaque).setMaxWidth(columna1);
+        tablaPrincipal.getColumnModel().getColumn(columnaEmpaque).setMinWidth(columna1/2);
+        tablaPrincipal.getColumnModel().getColumn(columnaEmpaque).setPreferredWidth(columna1);
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaBodega).setMaxWidth(columna1);
+        tablaPrincipal.getColumnModel().getColumn(columnaBodega).setMinWidth(columna1/2);
+        tablaPrincipal.getColumnModel().getColumn(columnaBodega).setPreferredWidth(columna1);
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaCantidad).setMaxWidth(columna1*3/2);
+        tablaPrincipal.getColumnModel().getColumn(columnaCantidad).setMinWidth(columna1);
+        tablaPrincipal.getColumnModel().getColumn(columnaCantidad).setPreferredWidth(columna1*3/2);
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaVunitario).setMaxWidth(columna1*2);
+        tablaPrincipal.getColumnModel().getColumn(columnaVunitario).setMinWidth(columna1);
+        tablaPrincipal.getColumnModel().getColumn(columnaVunitario).setPreferredWidth(columna1*2);
+        
+        tablaPrincipal.getColumnModel().getColumn(columnaVtotal).setMaxWidth(columna1*5/2);
+        tablaPrincipal.getColumnModel().getColumn(columnaVtotal).setMinWidth(columna1*2);
+        tablaPrincipal.getColumnModel().getColumn(columnaVtotal).setPreferredWidth(columna1*5/2);
+	}
     
     @Override
     public void actualizarFrame() {
