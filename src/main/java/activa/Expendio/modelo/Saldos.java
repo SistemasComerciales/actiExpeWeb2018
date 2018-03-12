74014f6d8ca38852383a3c67806c7b55f07d3fb7
Administrador <Administrador@192.168.5.1>
2018-03-11 21:43:55 -0500

    Adicion Saldos. Validacion de Saldos en el interno.

---
+++ src/main/java/activa/Expendio/modelo/Saldos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activa.Expendio.modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Saldos {
+    public static final long saldoDiarioMaximo = 52082;
+    public static final long saldoMensualMaximo = 1562484;
+}
